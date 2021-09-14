875ba776999c43c8ad7bc00ff582a26bde1f3fe8
Oğuzhan <58344612+okutucu@users.noreply.github.com>
2021-09-14 23:02:45 +0300

    updated project.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductAdd @add:product="addProduct"/>
+    <ProductList @delete:product="deleteProduct" @update:product="updateProduct" :products="products"/>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import ProductList from "./components/ProductList";
+import ProductAdd from "./components/ProductAdd";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductAdd,
+    ProductList
+
+  },
+  data() {
+    return {
+      products: []
+  }
+  },
+  mounted(){
+    this.getProducts()
+  },
+  methods : {
+    async getProducts()
+    {
+      const result = await fetch('http://localhost:3000/products') // http result olarak dönecek
+      const data = await  result.json()
+      this.products =data;
+    },
+    async deleteProduct(product){
+      await fetch('http://localhost:3000/products/'+product.id,{
+        method:'DELETE'
+      })
+      this.products = this.products.filter(
+          productToFilter => productToFilter.id!==product.id
+      )
+    },
+    async updateProduct(product){
+      const result = await fetch('http://localhost:3000/products/'+product.id,{
+        method:'PUT',
+        body:JSON.stringify(product),
+        headers:{"Content-Type":"application/json"}
+      })
+      const updatedProduct = await result.json()
+      this.products = this.products.map(product=> product.id===updatedProduct.id?updatedProduct:product)
+    },
+    async addProduct(product){
+       const result = await fetch('http://localhost:3000/products',{
+        method:'POST',
+        body:JSON.stringify(product),
+        headers:{"Content-Type":"application/json"}
+      })
+      const newProduct = await result.json()
+      this.products=[...this.products,newProduct]
+    }
   }
 }
 </script>
@@ -21,7 +68,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
